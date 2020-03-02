c2f3a266a8afaf58e73eccfe8a5b34e3b7009ba3
Carlos Manrique Enguita <UO258425@uniovi.es>
2020-03-02 11:59:42 +0100

    Updates

---
+++ precache-manifest.4ebba3a263ecc5f277b577091f22230f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6502258987da8fe3b7fc83d296ac304c",
+    "url": "/npm-example/index.html"
+  },
+  {
+    "revision": "367e6013db8d8fe726fa",
+    "url": "/npm-example/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5f43afdda7ca321c3b92",
+    "url": "/npm-example/static/js/2.28d221d8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/npm-example/static/js/2.28d221d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367e6013db8d8fe726fa",
+    "url": "/npm-example/static/js/main.deb59fd7.chunk.js"
+  },
+  {
+    "revision": "46557b3cca40b5426c30",
+    "url": "/npm-example/static/js/runtime-main.eab51f1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/npm-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
